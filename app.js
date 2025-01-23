f74cdc54b631016a30bd481936f9cb6aa5fd5403
Buki Filani <127331097+Bookie-Git@users.noreply.github.com>
2025-01-23 10:52:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Family mi, alaafia! Oluwaseun!!!'));
+app.get('/', (req, res) => res.send('E ti tobi to o Jesu! Oluwaseun!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
